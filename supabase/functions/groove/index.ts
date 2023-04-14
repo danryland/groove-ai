69606289eef20e89fa43c81b7e5b37db8f54e864
Dan Ryland <dan@ryland.consulting>
2023-04-14 18:03:35 +0100

    add groove function

--- supabase/functions/groove/index.ts
+++ supabase/functions/groove/index.ts
@@ -2,17 +2,33 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
+import "https://deno.land/x/xhr@0.3.0/mod.ts";
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+import { Configuration, OpenAIApi } from "https://esm.sh/openai@3.2.1";
 
-console.log("Hello from Functions!");
+const configuration = new Configuration({
+  apiKey: Deno.env.get("OPENAI_API_KEY"),
+});
+
+const openai = new OpenAIApi(configuration);
 
 serve(async (req) => {
-  const { name } = await req.json();
-  const data = {
-    message: `Hello ${name}!`,
-  };
+  const prompt =
+    "1. For 'genre', Select one of the following genres: dance, funk, pop, rnb or trap. 2. For 'title', give me a three word title for a drum rhythm matching the genre you've selected. 3. For 'description', give me a two line rap about your title without including the words of the title. 4. For 'beat_high' 'beat_med' and 'beat_low', give me a high, mid and low rhythm to match the genre and title. Each is an array of 8 objects, where each object has an 'active' property. The 'active' property value alternates between true and false. Return each answer in one json object.";
 
-  return new Response(JSON.stringify(data), {
+  const response = await openai.createChatCompletion({
+    model: "gpt-4",
+    messages: [{ role: "user", content: prompt }],
+    max_tokens: 300,
+  });
+
+  const {
+    data: {
+      choices: [{ message }],
+    },
+  } = response;
+
+  return new Response(message?.content, {
     headers: { "Content-Type": "application/json" },
   });
 });
