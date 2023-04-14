6c38d7faaf08ea0e5c9b3b4b6855237b74d5b336
Dan Ryland <dan@ryland.consulting>
2023-04-14 18:14:02 +0100

    add error check in function

--- supabase/functions/groove/index.ts
+++ supabase/functions/groove/index.ts
@@ -13,28 +13,35 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 serve(async (req) => {
-  const prompt =
-    "1. For 'genre', Select one of the following genres: dance, funk, pop, rnb or trap. 2. For 'title', give me a three word title for a drum rhythm matching the genre you've selected. 3. For 'description', give me a two line rap about your title without including the words of the title. 4. For 'beat_high' 'beat_med' and 'beat_low', give me a high, mid and low rhythm to match the genre and title. Each is an array of 8 objects, where each object has an 'active' property. The 'active' property value alternates between true and false. Return each answer in one json object.";
+  try {
+    const prompt =
+      "1. For 'genre', Select one of the following genres: dance, funk, pop, rnb or trap. 2. For 'title', give me a three word title for a drum rhythm matching the genre you've selected. 3. For 'description', give me a two line rap about your title without including the words of the title. 4. For 'beat_high' 'beat_med' and 'beat_low', give me a high, mid and low rhythm to match the genre and title. Each is an array of 8 objects, where each object has an 'active' property. The 'active' property value alternates between true and false. Return each answer in one json object.";
 
-  const response = await openai.createChatCompletion({
-    model: "gpt-4",
-    messages: [{ role: "user", content: prompt }],
-    max_tokens: 300,
-  });
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      max_tokens: 300,
+    });
 
-  const {
-    data: {
-      choices: [{ message }],
-    },
-  } = response;
+    const {
+      data: {
+        choices: [{ message }],
+      },
+    } = response;
 
-  return new Response(message?.content, {
-    headers: { "Content-Type": "application/json" },
-  });
+    return new Response(message?.content, {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error in function:", error);
+    return new Response(JSON.stringify({ error: "An error occurred" }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
+    });
+  }
 });
 
 // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+//   --header 'Content-Type: application/json'
