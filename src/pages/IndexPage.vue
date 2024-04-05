c4e4b87e9df5a46a6c75a5a51c8af2b81bd14228
Dan Ryland <dan@ryland.consulting>
2024-04-05 01:19:14 +0100

    add advertise link

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,89 +9,45 @@
 
     <div class="beats">
       <div class="beat beat-high">
-        <button
-          v-for="(high, index) in groove.beat_high"
-          :key="'high-' + index"
-          :class="{ active: high.active, current: index === currentBeat }"
-          @click="toggleActive(high)"
-        ></button>
+        <button v-for="(high, index) in groove.beat_high" :key="'high-' + index"
+          :class="{ active: high.active, current: index === currentBeat }" @click="toggleActive(high)"></button>
       </div>
       <div class="beat beat-mid">
-        <button
-          v-for="(mid, index) in groove.beat_mid"
-          :key="'mid-' + index"
-          :class="{ active: mid.active, current: index === currentBeat }"
-          @click="toggleActive(mid)"
-        ></button>
+        <button v-for="(mid, index) in groove.beat_mid" :key="'mid-' + index"
+          :class="{ active: mid.active, current: index === currentBeat }" @click="toggleActive(mid)"></button>
       </div>
       <div class="beat beat-low">
-        <button
-          v-for="(low, index) in groove.beat_low"
-          :key="'low-' + index"
-          :class="{ active: low.active, current: index === currentBeat }"
-          @click="toggleActive(low)"
-        ></button>
+        <button v-for="(low, index) in groove.beat_low" :key="'low-' + index"
+          :class="{ active: low.active, current: index === currentBeat }" @click="toggleActive(low)"></button>
       </div>
     </div>
 
     <div class="bpm">
-      <q-btn
-        icon="fa-solid fa-minus"
-        round
-        flat
-        :disable="bpm <= 20"
-        @click="down(bpm)"
-      />
-      <q-input
-        v-model.number="bpm"
-        @change="updateBPM"
-        min="20"
-        max="300"
-        step="2"
-        type="number"
-        borderless
-      />
-      <q-btn
-        icon="fa-solid fa-plus"
-        round
-        flat
-        :disable="bpm >= 300"
-        @click="up(bpm)"
-      />
+      <q-btn icon="fa-solid fa-minus" round flat :disable="bpm <= 20" @click="down(bpm)" />
+      <q-input v-model.number="bpm" @change="updateBPM" min="20" max="300" step="2" type="number" borderless />
+      <q-btn icon="fa-solid fa-plus" round flat :disable="bpm >= 300" @click="up(bpm)" />
     </div>
     <div class="action">
-      <div class="box box-1">&nbsp;</div>
+      <div class="box box-1"><a
+          href="mailto:dan@ryland.consulting?subject=Interested in advertising on Groove AI">Advertise</a></div>
       <div class="box box-2">
         <q-btn @click="playSequence" :disabled="!hasActiveBeat" round size="lg">
-          <q-icon
-            size="1.5em"
-            v-if="playbackState === 'playing'"
-            name="fa-sharp fa-solid
-        fa-pause"
-          />
-          <q-icon
-            v-else
-            size="1.5em"
-            name="fa-sharp fa-solid
-        fa-play"
-          />
+          <q-icon size="1.5em" v-if="playbackState === 'playing'" name="fa-sharp fa-solid
+        fa-pause" />
+          <q-icon v-else size="1.5em" name="fa-sharp fa-solid
+        fa-play" />
         </q-btn>
       </div>
       <div class="box box-3">
-        <q-btn
-          @click="downloadMIDI"
-          rounded
-          class="q-px-md"
-          color="white"
-          outline
-          ><q-icon
-            size="1.2em"
-            name="fa-sharp fa-light fa-arrow-down"
-            class="q-mr-sm"
-          />
-          <span>MIDI</span></q-btn
-        >
+        <q-btn @click="downloadMIDI" rounded class="q-px-md" color="white" outline><q-icon size="1.2em"
+            name="fa-sharp fa-light fa-arrow-down" class="q-mr-sm" />
+          <span>MIDI</span></q-btn>
       </div>
+    </div>
+    <div class="full-width text-center">
+      <p class="q-mb-none text-grey-5" style="line-height: 1.2em;"><small>Project by <a class="text-grey-5"
+            target="_blank" href="https://twitter.com/realdanryland">Dan Ryland</a><br>co-founder of <a
+            class="text-grey-5" target="_blank" href="https://insidrmusic.com">Insidr&nbsp;Music</a></small></p>
     </div>
   </q-page>
 </template>
